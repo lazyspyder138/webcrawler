e9be524f866fb3e08795198a69a4700f23144c33
HARISH TIWARI <48086629+lazyspyder138@users.noreply.github.com>
2022-09-15 22:44:17 +0530

    Delete tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-nter your website url:\n eg: https://www.nseindia.com/get-quotes/equity?symbol=JINDALSTEL")
-    # print("This crawler is only for nse website:")
-    # website_url = input()
-    # print("Pls provide the start date and end date")
-    # start_date = input("Start Date in (dd-mm-yyy) formate: ")
-    # end_date = input("En
